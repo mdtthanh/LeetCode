1d76ac74c2b0bb46638b70f330f9b6f4291c9f8d
mdtthanh <maithanh0131@gmail.com>
2022-08-10 11:29:40 +0700

    11:29 10/8

---
+++ 1475-FinalPricesWithaSpecialDiscoun.js
@@ -0,0 +1,11 @@
+var finalPrices = function(prices) {
+    for(var i = 0; i < prices.length-1; i++ ){
+        for(var j = i+1; j < prices.length; j++ ){
+            if(prices[i] >= prices[j]){
+                prices[i] = prices[i] - prices[j]
+                break
+            }
+        }
+    }
+    return prices
+};
