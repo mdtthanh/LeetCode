4fa51f861842d1af76d365472012ade20bcbd9ad
mdtthanh <maithanh0131@gmail.com>
2022-08-13 16:33:29 +0700

    16:33 13/8/2022

---
+++ Easy/1464-MaximumProductofTwoElementsinanArray.js
@@ -0,0 +1,11 @@
+var maxProduct = function(nums) {
+    let max = 0
+
+    for(let i=0; i<nums.length-1; i++){
+        for(let j=i+1; j<nums.length; j++){
+            max = Math.max(max, (nums[i]-1)*(nums[j]-1))
+        }   
+    }   
+    
+    return max
+};
