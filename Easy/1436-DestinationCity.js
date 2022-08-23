c01f4755b33a136799baf232e01176d0e9675ab1
mdtthanh <maithanh0131@gmail.com>
2022-08-23 23:06:47 +0700

    23:06 23/8/2022

---
+++ Easy/1436-DestinationCity.js
@@ -0,0 +1,11 @@
+var destCity = function(paths) {
+ 
+    let map={}
+    
+    for(let city of paths){
+        map[city[0]]=map[city[0]]?map[city[0]]+1:1
+    }
+    for(let city of paths){
+        if(!map[city[1]]) return city[1]
+    }
+};
